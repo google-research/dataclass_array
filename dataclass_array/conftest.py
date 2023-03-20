a3d99c798d653cd1358c2917dea27f92ebe4ed7f
Etienne Pot <epot@google.com>
2023-03-20 09:02:43 -0700

    Release dataclass_array 1.4.1 (Pytorch 2.0 support)
    PiperOrigin-RevId: 517983642

---
+++ dataclass_array/conftest.py
@@ -0,0 +1,23 @@
+# Copyright 2022 The dataclass_array Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Pytest configuration."""
+
+# Pytest crash when the lazy-imports are triggered inside the tests. Likely
+# due to xdist. Instead, import them during collect.
+# pylint: disable=unused-import
+import jax
+import tensorflow
+import torch
+# pylint: enable=unused-import
