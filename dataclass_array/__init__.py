31da4892002ecf082b77dc0104c1955dfc5327a6
Etienne Pot <epot@google.com>
2024-03-19 06:59:58 -0700

    Release dataclass_array 1.5.2
    PiperOrigin-RevId: 617158809

--- dataclass_array/__init__.py
+++ dataclass_array/__init__.py
@@ -44,6 +44,6 @@ from dataclass_array import testing
 
 # A new PyPI release will be pushed everytime `__version__` is increased
 # When changing this, also update the CHANGELOG.md
-__version__ = '1.5.1'
+__version__ = '1.5.2'
 
 del sys, pytest
